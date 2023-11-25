2583068d5534c49c9238a3201dc1f5659f3d7589
TouchSaven <touchsaven152@gmail.com>
2023-11-25 16:12:15 +0700

    first

---
+++ firstproject/Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstproject.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Show()
+            
+        {
+            return View();
+        }
+        public ActionResult Shows()
+
+        {
+            return View("Show");
+        }
+        public ActionResult Showm()
+
+        {
+            return View();
+        }
+        public ActionResult Showc()
+
+        {
+            return View("~/Views/About/Add.cshtml");
+        }
+        public ContentResult showContent()
+        {
+            return Content("Hello");
+        }
+
+    }
+}
