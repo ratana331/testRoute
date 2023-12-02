8bbb8fadc4184265367afa62b180313c97d8b3f1
TouchSaven <touchsaven152@gmail.com>
2023-12-02 10:12:35 +0700

    Add Document Controller

---
+++ firstproject/Controllers/DocumentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstproject.Controllers
+{
+    public class DocumentController : Controller
+    {
+        // GET: Document
+        public ActionResult Saven()
+        {
+            return File(Server.MapPath("~/Content/test.txt"),"text/pain");
+        }
+        public FileResult Downlowd()
+        {
+            return File(Server.MapPath("~/Content/test.txt"), "test/pain", "kim.txt");
+        }
+    }
+}
