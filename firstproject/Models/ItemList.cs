a1378f8d149f1a527e7e1ab1f4a0eb850db3409d
TouchSaven <touchsaven152@gmail.com>
2023-12-02 16:48:08 +0700

    Item

---
+++ firstproject/Models/ItemList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace firstproject.Models
+{
+    public class ItemList
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Category { get; set; }
+    }
+}
