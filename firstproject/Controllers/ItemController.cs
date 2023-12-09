33a0e1d3b3f6336ac4961f344c932a1ee0b897ad
TouchSaven <touchsaven152@gmail.com>
2023-12-09 15:14:44 +0700

    test with model

--- firstproject/Controllers/ItemController.cs
+++ firstproject/Controllers/ItemController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using firstproject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,16 @@ namespace firstproject.Controllers
         // GET: Item
         public ActionResult Index()
         {
-            return View();
+            ViewBag.ItemList = "Computer Shop";
+            List<ItemList> itemList = new List<ItemList>()
+            {
+                new ItemList{ID="001",Name="Acer",Price=500,Category="Laptop"},
+                new ItemList{ID="002",Name="Mac",Price=1500,Category="Laptop"},
+                new ItemList{ID="003",Name="Asus",Price=600,Category="Laptop"},
+                new ItemList{ID="001",Name="Dell",Price=580,Category="Laptop"},
+            };
+            itemList.Add(new ItemList { ID = "005", Name = "MSI", Price = 900, Category = "Laptop" });
+            return View(itemList);
         }
     }
 }
--- firstproject/Models/ItemList.cs
+++ firstproject/Models/ItemList.cs
@@ -11,5 +11,7 @@ namespace firstproject.Models
         public string Name { get; set; }
         public float Price { get; set; }
         public string Category { get; set; }
+
+       
     }
 }
