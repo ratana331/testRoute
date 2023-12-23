dc3bce8403d0b0f7ff3b1e9dfd5e63e6358c7d02
ratana331 <racharatana@gmail.com>
2023-12-23 16:48:38 +0700

    testRoute

--- firstproject/App_Start/RouteConfig.cs
+++ firstproject/App_Start/RouteConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -12,12 +13,27 @@ namespace firstproject
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes(); //enable attribute route
+
+         routes.MapRoute(
+         name: "display",
+         url: "display/{cid}",
+         constraints: new {cid=@"\d{1,3}"},
+         defaults: new { controller = "Contact", action = "Show", id = UrlParameter.Optional }
+     );
+
+            routes.MapRoute(
+              name: "Contact",
+              url: "contact",
+              defaults: new { controller = "Item", action = "Index", id = UrlParameter.Optional }
+          );
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
+            
         }
     }
 }
---
+++ firstproject/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstproject.Controllers
+{
+    [RoutePrefix("admin")]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [Route("account")]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("display")]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [Route("letgo")]
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [Route(@"display/{id:range(1,100)}/{qty:regex(\d{3})}")]
+        public ActionResult Show(int id, int qty)
+        {
+            return View();
+        }
+    }
+}
